619b3c2b13c5e8ce8a9fe4ae729b23914fff05a6
Roshin Abdul Majeed <abuzayd@Roshins-MacBook-Pro.local>
2015-09-25 19:23:43 +0400

    Schedule status page for desktop app ready

--- js/js.js
+++ js/js.js
@@ -49,77 +49,86 @@ function whichanimationEvent(){
 var animationEvent = whichanimationEvent();
 
 
-// TweenMax
-var repeatDelay = 3;  // seconds
-var repeated = 100; // times
+// TimelineMax
+var tl = new TimelineMax({repeat:-1});
 
-TweenMax.staggerFromTo('g#prlogotextgroup path', 2, {
-  opacity:0,
-  x:0, y:-10,
-  scale: 1.5,
-  ease:Elastic.easeIn,
-  repeat: repeated,
-  repeatDelay: repeatDelay,
-  yoyo: true
+tl.from('path#prlogo-pee', 2,  {
+  opacity: 0,
+  y:-20,
+  scale: 2,
+  ease:Elastic.easeOut
+}, "pee")
+.fromTo('path#prlogo-smallwave', 2,  {
+  opacity: 0,
+  x:-10,
+  y:15,
+  ease:Elastic.easeOut.config(1, 0.5),
+  scale: 0.7
+}, {
+  opacity: 1,
+  x:0,
+  y:0,
+  ease:Elastic.easeOut.config(1, 0.5),
+  scale: 1
+}, "pee+=0.4")
+.fromTo('path#prlogo-bigwave', 2,  {
+  opacity: 0,
+  x:-10,
+  y:15,
+  ease:Elastic.easeOut.config(1, 0.5),
+  scale: 0.7
+}, {
+  opacity: 1,
+  x:0,
+  y:0,
+  ease:Elastic.easeOut.config(1, 0.5),
+  scale: 1
+}, "pee+=0.8")
+.staggerFromTo('g#prlogotextgroup path', 1, {
+  x:-20, y:5,
+  scale: 0.8,
+  ease:Elastic.easeIn.config(1, 0.5)
 }, {
   opacity:1,
   x:0, y:0,
   scale: 1,
-  ease:Elastic.easeIn,
-  repeat: repeated,
-  repeatDelay: repeatDelay,
-  yoyo: true
-}, 0.05);
+  ease:Elastic.easeIn.config(1, 0.5)
+}, 0.05, "pee+=0.5")
 
-TweenMax.from('path#prlogo-pee', 2,  {
-  opacity: 0,
-  y:10,
-  ease:Elastic.easeOut,
-  repeat: repeated,
-  repeatDelay: repeatDelay,
-  yoyo: true
-});
+/*delay*/
 
-TweenMax.fromTo('path#prlogo-smallwave', 2,  {
+.to('', 6, {})
+
+/*going back*/
+
+.staggerTo($('g#prlogotextgroup path').get().reverse(), 1, {
   opacity: 0,
-  x:-10,
-  y:15,
-  ease:Elastic.easeOut.config(1, 0.5),
-  scale: 0.7,
-  delay: 0.2,
-  repeat: repeated,
-  repeatDelay: repeatDelay,
-  yoyo: true
-}, {
-  opacity: 1,
-  x:0,
-  y:0,
-  ease:Elastic.easeOut.config(1, 0.5),
+  x:20, y:0,
   scale: 1,
-  delay: 0.2,
-  repeat: repeated,
-  repeatDelay: repeatDelay,
-  yoyo: true
-});
+  ease:Elastic.easeIn.config(1, 0.5)
+}, 0.05, "goingBack")
 
-TweenMax.fromTo('path#prlogo-bigwave', 2,  {
+.to('path#prlogo-bigwave', 0.5,  {
   opacity: 0,
-  x:-10,
-  y:15,
-  ease:Elastic.easeOut.config(1, 0.5),
-  scale: 0.7,
-  delay: 0.4,
-  repeat: repeated,
-  repeatDelay: repeatDelay,
-  yoyo: true
-}, {
-  opacity: 1,
-  x:0,
-  y:0,
-  ease:Elastic.easeOut.config(1, 0.5),
-  scale: 1,
-  delay: 0.4,
-  repeat: repeated,
-  repeatDelay: repeatDelay,
-  yoyo: true
-});
+  x:20,
+  y:-9,
+  ease: Power4.easeInOut,
+  scale: 1.2 },
+   "-=0.25")
+.to('path#prlogo-smallwave', 0.5,  {
+  opacity: 0,
+  x:20,
+  y:-9,
+  ease: Power4.easeInOut,
+  scale: 1.2 },
+  "-=0.25")
+.to('path#prlogo-pee', 0.5,  {
+  opacity: 0,
+  x:20,
+  y:-9,
+  ease: Power4.easeInOut,
+  scale: 1.2 },
+  "-=0.25")
+/*delay*/
+
+.to('', 1, {});
